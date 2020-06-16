5bbdd0e1ed2359163b3e3731d7b7a9c8119bf123
cheol-hee-Lee <201802141@o.cnu.ac.kr>
2020-06-16 23:00:50 +0900

    Add kiwoom login

---
+++ ui/ui.py
@@ -0,0 +1,13 @@
+from kiwoom.kiwoom import *
+from PyQt5.QtWidgets import *
+import sys
+
+class Ui_class():
+    def __init__(self):
+        print("UI 입니다.")
+
+        self.app = QApplication(sys.argv)
+
+        self.kiwoom = Kiwoom()
+
+        self.app.exec_()
